03dcef8a7c6fec80319672373134264156910e83
lxdlxd122 <823801536@qq.com>
2020-02-25 19:28:31 +0800

    3333

--- app/src/androidTest/java/com/example/miner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/miner/ExampleInstrumentedTest.java
@@ -23,7 +23,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.miner", appContext.getPackageName());
-        assertEquals("com.example.miner", appContext.getPackageName());
-        assertEquals("com.example.miner", appContext.getPackageName());
     }
 }
