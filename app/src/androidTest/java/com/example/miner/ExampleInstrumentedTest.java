918106ae6057b08739841914cce1be8a45a76366
lxdlxd122 <823801536@qq.com>
2020-02-27 11:31:39 +0800

    苏晓阔

--- app/src/androidTest/java/com/example/miner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/miner/ExampleInstrumentedTest.java
@@ -23,5 +23,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.miner", appContext.getPackageName());
+        assertEquals("com.example.miner", appContext.getPackageName());
     }
 }
